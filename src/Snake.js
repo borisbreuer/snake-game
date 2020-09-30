4f0c4bfa821af90d9b7620dc01bbd4ebe744674d
Boris Breuer <boris.breuer@respondi.com>
2020-09-30 12:27:42 +0200

    gameplay

--- src/Snake.js
+++ src/Snake.js
@@ -2,64 +2,86 @@ import { canvas, c, log } from "./helper";
 
 export default class Snake {
   constructor(x, y, r, color) {
+    this.offset = 2;
     this.x = x;
     this.y = y;
-    this.r = r;
+    this.r = r - this.offset;
     this.color = color;
     this.vel = {
-      x: this.r * 2,
-      y: this.r * 2
+      x: (this.r + this.offset) * 2,
+      y: (this.r + this.offset) * 2
     };
 
     this.alpha = 1;
     this.snake = [];
     this.bodyLength = 0;
+    this.dirVal = "";
   }
 
   draw() {
     for (let i = 0; i < this.snake.length; i++) {
+      if (i != this.snake.length - 1) {
+      }
+      let modulo = i % 2;
+      let color = modulo == 0 ? [0, 100, 0] : [0, 150, 0];
       let pos = this.snake[i];
-      c.fillStyle = `rgba(0,127,0, ${(0.75 / this.snake.length) * (i + 4)})`;
+      c.fillStyle = `rgba(${color}, ${(0.75 / this.snake.length) * (i + 4)})`;
       c.lineWidth = 1;
       c.strokeStyle = `rgba(0,200,0, 1)`;
       c.beginPath();
-      c.arc(pos.x + this.r, pos.y + this.r, this.r, 0, Math.PI * 2, false);
+      c.arc(
+        pos.x + this.r + this.offset,
+        pos.y + this.r + this.offset,
+        this.r,
+        0,
+        Math.PI * 2,
+        false
+      );
       c.stroke();
       c.fill();
       c.closePath();
     }
 
-    c.fillStyle = `rgba(0,255,0, ${this.alpha})`;
+    c.fillStyle = `rgba(20,200,20, ${this.alpha})`;
+    c.lineWidth = 1;
+    c.strokeStyle = `rgba(0,100,0, 1)`;
     c.beginPath();
-    c.arc(this.x + this.r, this.y + this.r, this.r, 0, Math.PI * 2, false);
+    c.arc(
+      this.x + this.r + this.offset,
+      this.y + this.r + this.offset,
+      this.r,
+      0,
+      Math.PI * 2,
+      false
+    );
+    c.stroke();
     c.fill();
     c.closePath();
   }
 
-  update(dir) {
+  update() {
+    if (this.bodyLength > 0) this.createBody();
+    this.move();
     this.draw();
-    this.move(dir);
-    this.createBody(dir);
   }
 
-  move(dir) {
-    // this.snake.pop();
-    if (dir == "LEFT" && dir != "RIGHT") {
+  move() {
+    if (this.dirVal == "LEFT" && this.dirVal != "RIGHT") {
       this.x -= this.vel.x;
-    } else if (dir == "UP" && dir != "DOWN") {
+    } else if (this.dirVal == "UP" && this.dirVal != "DOWN") {
       this.y -= this.vel.y;
-    } else if (dir == "RIGHT" && dir != "LEFT") {
+    } else if (this.dirVal == "RIGHT" && this.dirVal != "LEFT") {
       this.x += this.vel.x;
-    } else if (dir == "DOWN" && dir != "UP") {
+    } else if (this.dirVal == "DOWN" && this.dirVal != "UP") {
       this.y += this.vel.y;
-    } else if (dir == "STOP") {
+    } else if (this.dirVal == "STOP") {
       // Stopp
     }
   }
 
-  createBody(dir) {
-    if (dir != "STOP") {
-      if (this.snake.length > this.bodyLength) {
+  createBody() {
+    if (this.dirVal != "STOP") {
+      if (this.snake.length >= this.bodyLength) {
         this.snake.splice(0, 1);
       }
       this.snake.push({ x: this.x, y: this.y });
@@ -79,7 +101,7 @@ export default class Snake {
   }
 
   borderCollision() {
-    if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
+    if (this.x < 0 || this.x >= canvas.width || this.y < 0 || this.y >= canvas.height) {
       return true;
     }
     return false;
--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import Snake from "./Snake";
 canvas.width = 600;
 canvas.height = 600;
 
+let gameState = "RUN";
 let score = 0;
-let dir = null;
 let div = 20;
 let countCol = 0;
 let countRow = 0;
 let foodPos;
 
+let scoreEl = document.querySelector("#scoreEl");
 const snake = new Snake(0, 0, 10, [200, 200, 200]);
 
 const randomFoodPos = () => {
@@ -22,8 +23,8 @@ const randomFoodPos = () => {
 foodPos = randomFoodPos();
 
 const spawnFood = () => {
-  c.fillStyle = "rgb(200,20,20)";
-  c.fillRect(foodPos.x * div, foodPos.y * div, div, div);
+  c.fillStyle = "rgb(255,10,10)";
+  c.fillRect(foodPos.x * div + 2, foodPos.y * div + 2, div - 4, div - 4);
 };
 
 const gameOver = () => {
@@ -35,30 +36,30 @@ const gameOver = () => {
   let tWidth = c.measureText(txt).width / 2;
 
   let gradient = c.createLinearGradient(canvas.width / 2 - tWidth, 0, canvas.width / 2 + tWidth, 0);
-  gradient.addColorStop("0", "hsl(327, 100%, 60%)");
-  gradient.addColorStop("0.5", "hsl(216, 100%, 50%)");
-  gradient.addColorStop("1", "hsl(193, 100%, 60%)");
+  gradient.addColorStop("0", "rgb(0, 200, 0)");
+  gradient.addColorStop("0.5", "rgb(255, 127, 0)");
+  gradient.addColorStop("1", "rgb(0, 200, 0)");
 
   c.shadowColor = "black";
   c.shadowBlur = 15;
 
-  c.strokeStyle = gradient;
+  c.fillStyle = gradient;
   c.lineWidth = 2;
-  c.strokeText(txt, canvas.width / 2 - tWidth, canvas.height / 2 + 20);
+  c.fillText(txt.toLocaleUpperCase(), canvas.width / 2 - tWidth, canvas.height / 2 + 20);
   c.restore();
 };
 
 const bgGrid = () => {
-  c.fillStyle = "rgb(0,0,0)";
+  c.fillStyle = "rgb(200,127,50)";
   c.fillRect(0, 0, canvas.width, canvas.height);
-
+  let color = "rgba(255,255,255,0.1)";
   for (let i = 0; i < canvas.width; i++) {
     for (let j = 0; j < canvas.height; j++) {
       if (countCol % 2 == 0 && countRow % 2 == 1) {
-        c.fillStyle = "rgb(20,20,20)";
+        c.fillStyle = color;
         c.fillRect(i * div, j * div, div, div);
       } else if (countCol % 2 == 1 && countRow % 2 == 0) {
-        c.fillStyle = "rgb(20,20,20)";
+        c.fillStyle = color;
         c.fillRect(i * div, j * div, div, div);
       }
       countRow++;
@@ -68,38 +69,42 @@ const bgGrid = () => {
 };
 
 const game = setInterval(() => {
-  if (snake.tailCollision()) {
-    log("GAME OVER");
+  if (gameState == "STOP") clearInterval(game);
+  bgGrid();
+
+  if (gameState == "STOP") {
     gameOver();
-    clearInterval(game);
+    return;
+  }
+  snake.update();
+  if (snake.x == foodPos.x * div && snake.y == foodPos.y * div) {
+    foodPos = randomFoodPos();
+    snake.addBody();
+    score++;
+    scoreEl.textContent = score;
+  }
+  spawnFood();
+
+  if (snake.tailCollision()) {
+    gameState = "STOP";
   }
   if (snake.borderCollision()) {
-    log("Game OVER");
-    gameOver();
-    clearInterval(game);
+    gameState = "STOP";
   }
-  bgGrid();
-  spawnFood();
-  snake.update(dir);
+}, 300);
 
-  if (snake.x == foodPos.x * div && snake.y == foodPos.y * div) {
-    score++;
-    snake.addBody();
-    foodPos = randomFoodPos();
-  }
-}, 150);
-
-const direction = e => {
-  if (e.key == "ArrowLeft" && dir != "RIGHT") {
-    dir = "LEFT";
-  } else if (e.key == "ArrowUp" && dir != "DOWN") {
-    dir = "UP";
-  } else if (e.key == "ArrowRight" && dir != "LEFT") {
-    dir = "RIGHT";
-  } else if (e.key == "ArrowDown" && dir != "UP") {
-    dir = "DOWN";
+function direction(e) {
+  if (e.key == "ArrowLeft" && snake.dirVal != "RIGHT") {
+    snake.dirVal = "LEFT";
+  } else if (e.key == "ArrowUp" && snake.dirVal != "DOWN") {
+    snake.dirVal = "UP";
+  } else if (e.key == "ArrowRight" && snake.dirVal != "LEFT") {
+    snake.dirVal = "RIGHT";
+  } else if (e.key == "ArrowDown" && snake.dirVal != "UP") {
+    snake.dirVal = "DOWN";
   } else if (e.key == " ") {
-    dir = "STOP";
+    snake.dirVal = "STOP";
   }
-};
-document.addEventListener("keydown", direction);
+}
+
+document.addEventListener("keydown", direction, false);
