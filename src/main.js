b8e9409f2f7a5331eae5045cd142f0feca10933f
Boris Breuer <mail@borisbreuer.de>
2020-10-13 19:12:24 +0200

    mobile proto

--- src/main.js
+++ src/main.js
@@ -79,3 +79,48 @@ function direction(e) {
 }
 
 document.addEventListener("keydown", direction, false);
+
+const btnUP = document.getElementById('btn_up');
+btnUP.addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  if(snake.dirVal != "DOWN") snake.dirVal = "UP";
+}, false);
+
+const btnLEFT = document.getElementById('btn_left');
+btnLEFT.addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  if(snake.dirVal != "RIGHT") snake.dirVal = "LEFT";
+}, false);
+
+const btnDOWN = document.getElementById('btn_down');
+btnDOWN.addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  if(snake.dirVal != "UP") snake.dirVal = "DOWN";
+}, false);
+
+const btnRIGHT = document.getElementById('btn_right');
+btnRIGHT.addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  if(snake.dirVal != "LEFT") snake.dirVal = "RIGHT";
+}, false);
+
+const btnStart = document.getElementById('btn_start');
+btnStart.addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  if (gameState == "STOP") {
+    init();
+    snake.dirVal = "STOP";
+  }
+}, false);
+
+const btnPause = document.getElementById('btn_pause');
+btnPause.addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  if (gameState == "RUN") {
+    // snake.dirVal = "STOP";
+    gameState = "PAUSE";
+  } else if (gameState == "PAUSE") {
+    animate();
+    gameState = "RUN";
+  }
+}, false);
